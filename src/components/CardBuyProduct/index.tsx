414a105337bd3f12675f002cb43c0322b72a1c9e
lucianoRib5 <luciano.eng.soft@outlook.com>
2023-01-31 23:27:48 -0300

    feat: add CardBuyProduct component

---
+++ src/components/CardBuyProduct/index.tsx
@@ -0,0 +1,8 @@
+const CardBuyProduct: React.FC = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default CardBuyProduct;
---
+++ src/components/CardBuyProduct/styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 285px;
+    width: 217px;
+    border-radius: 8px;
+    box-shadow: 0px 2px 8px 0px #00000022;
+`;
